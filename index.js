eaa5b6d9428013fe1542254312d6f23fa1bf9dc8
Leonildo Camilo <leonildo_camilo@hotmail.com>
2023-12-28 20:27:51 -0300

    adição de comentários

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ operation()
 function operation(){
 
 //inicia uma prompt copm algumas escolhas
+//vai criar um prompt, que é chamado assim que o programa é executado, é do tipo lista,e o choices é para escolher uma das alternativas
   inquirer.prompt([{
     type: 'list',
     name: 'action',
@@ -18,7 +19,8 @@ function operation(){
     choices: ['Criar Conta','Consultar Saldo','Depositar','Sacar','Sair']
   },//esse .then pega a action que foi escolhida, e exibe como resposta
   ]).then((answer) => {
-
+    //answer(resposta) é dado atraves do action, e ele que vai descidir qual função chamar, dependendo da escolha
+    //do usuário    
     const action = answer['action']
 
    if(action === 'Criar Conta'){
@@ -40,34 +42,40 @@ function operation(){
 
 //criando conta
 
+//essa função, serve apenas para exibit uma mensagem de parabens por criar a conta
 function createAccount() {
   console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco'))
   console.log(chalk.green('Defina as opções da sua conta a seguir')) 
 }
 
+//essa é a função que realmente vai estruturar a conta
 function buildAccount() {
 
+  //os requisitos para estruturar a conta são, o nome e armazenar em accountName
   inquirer.prompt([
     {
       name: 'accountName',
       message: 'Digite um nome para a sua conta'
     }
   ]).then((answer) => {
+    //aqui a resposta(answer) vai ser utilizado para criar uma conta, nesse nome
     const accountName = answer['accountName']
+    
 
     console.log(accountName)
-
+    //se não existir nenhuma conta, ele vai criar o repositório accounts
     if(!fs.existsSync('accounts')){
       fs.mkdirSync('accounts')
     }
 
+    //se o repositório accounts existir, ele vai criar uma conta no formato json
     if(fs.existsSync(`accounts/${accountName}.json`)){
       console.log(chalk.bgRed.black('Essa conta já existe, escolha outro nome!'),
       )
       buildAccount()
       return
     }
-
+    //da um valor inicial a conta criada, usando o Nome como parametro, de 0 Reais iniciais
     fs.writeFileSync(`accounts/${accountName}.json`,
      '{"balance": 0}',
       function(err) {console.log(err)})
@@ -78,6 +86,8 @@ function buildAccount() {
 }
 
 function deposit(){
+
+  //vai usar o nome da conta como parametro para acessar ela
   inquirer.prompt([
     {
       name:'accountName',
@@ -85,11 +95,12 @@ function deposit(){
     }
   ]).then((answer) => {
     const accountName = answer['accountName']
-
+    //se não existir conta com esse nome, ele vai perguntar novamente o nome da conta, ele checa se a conta existe
+    //utilizando a função de checagem de conta
     if(!checkAccount(accountName)) {
       return deposit()
     }
-
+    //pergunta a quantidade que vai ser depositado, e chama a função de depositar
     inquirer.prompt([
       {
         name:'amount',
@@ -110,6 +121,7 @@ function deposit(){
 }
 
 function checkAccount(accountName){
+  //ve se o nome da conta existe no depositorio accounts em formato json, se existir retorna verdadeiro, se não, falso
   if(!fs.existsSync(`accounts/${accountName}.json`)){
   console.log(chalk.bgRed.black('Esta conta não existe'))
   return false
@@ -119,15 +131,16 @@ return true
 }
 
 function addAmount( accountName, amount) {
-
+  //pega o nome da conta do usuário chamando essa função
   const accountData = getAccount(accountName)
 
+  //se o usuário não passar um valor valido, ele retorna uma menssagem de erro e retorna para o deposito
   if(!amount) {
     console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde'))
     return deposit()
   }
 
-  //pega o balance la na conta feita em json transforma em numero e adiciona a ela
+  //transforma a string em numero, e adiciona ao objeto balance no json somando ao valor ja existente lá
   accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
   fs.writeFileSync(`accounts/${accountName}.json`,
   JSON.stringify(accountData),
@@ -140,6 +153,7 @@ function addAmount( accountName, amount) {
 }
 
 function getAccount(accountName) {
+  //le o nome da conta do usuario na pagina accounts, flag r, quer dizer que ele vai ler, R de read
   const accountJSON= fs.readFileSync(`accounts/${accountName}.json`, {
     encoding: 'utf8',
     flag: 'r'
@@ -149,6 +163,8 @@ function getAccount(accountName) {
 }
 
 function getAccountBalance() {
+
+  //pega o nome da conta
   inquirer.prompt([
     {
       name:'accountName',
@@ -159,11 +175,11 @@ function getAccountBalance() {
     const accountName = answer['accountName']
 
     // verificando se account existe
-
     if(!checkAccount(accountName)){
       return getAccountBalance()
     }
 
+    // o valor da conta, vai ser igual ao balance da conta selecionada
     const accountData = getAccount(accountName)
 
     console.log(chalk.bgBlue.black(`Olá o saldo da sua conta é de R$${accountData.balance}`))
@@ -176,6 +192,7 @@ function getAccountBalance() {
 
 function widthdraw() {
 
+  //pergunta o nome da conta para acessar ela
   inquirer.prompt([
     {
       name:'accountName',
@@ -184,7 +201,7 @@ function widthdraw() {
   ]).then((answer) => {
 
     const accountName = answer['accountName']
-
+    //se a conta não existir pergunte novamente
     if(!checkAccount(accountName)){
       return widthdraw()
     }
@@ -196,6 +213,7 @@ function widthdraw() {
       }
     ]).then((answer) => {
 
+      //chama uma função para remover o valor da escolhido da conta
       const amount = answer['amount']
       removeAmount(accountName, amount)
     })
@@ -208,19 +226,22 @@ function widthdraw() {
 function removeAmount(accountName, amount) {
   const accountData = getAccount(accountName)
 
+  //se n tiver dinheiro, retorne uma menssagem de erro
   if(!amount) {
     console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde')
     )
     return widthdraw()
   }
 
+  //se tiver menos dinheiro do que quer sacar, fala q o valor não esta disponivel
   if(accountData.balance < amount) {
     console.log(chalk.bgRed.black('Valor indisponivel'))
     return widthdraw()
   }
 
+  //se tudo estiver correto, subtrai o balance, do valor desejado
   accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
-
+  //reescreve o novo valor no objeto balance do json
   fs.writeFileSync(
     `accounts/${accountName}.json`,
     JSON.stringify(accountData),
